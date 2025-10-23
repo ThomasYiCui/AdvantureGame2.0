b3305f60c4a4d57573875823c7537176f79d6638
ThomasYiCui <93477247+ThomasYiCui@users.noreply.github.com>
2025-10-22 22:07:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,36 @@ canvas.addEventListener("mouseup", function(e) {
 window.addEventListener("keydown", keysPressed, false);
 window.addEventListener("keyup", keysReleased, false);
 
+function Player() {
+    this.x = 0;
+    this.y = 0;
+    this.lvl = 0;
+    this.exp = 0;
+    this.size = 30;
+    this.spd = 3;
+}
+Player.prototype.draw = function() {
+    ctx.fillStyle = "rgb(255, 0, 0)";
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+}
+Player.prototype.update = function() {
+    if(keys[87] || keys[38]) {
+        this.y-=this.spd;
+    } else if(keys[83] || keys[40]) {
+        this.y+=this.spd;
+    }
+    if(keys[65] || keys[37]) {
+        this.x-=this.spd;
+    } else if(keys[68] || keys[39]) {
+        this.x+=this.spd;
+    }
+}
+
+var player = new Player();
 
 var update = setInterval(function() {
-  ctx.fillRect(0, 0, canvas.width * Math.random(), canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    player.draw();
 }, 20);
